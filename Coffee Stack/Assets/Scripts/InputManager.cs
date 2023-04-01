dbcd07b3561f88ba86dbf2b8b6298fc48e5a5813
gorkemkoksal <81961582+gorkemkoksal@users.noreply.github.com>
2023-04-01 20:03:04 +0300

    input

---
+++ Coffee Stack/Assets/Scripts/InputManager.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static Action<float> onAnyTouch;
+
+    public void Touch(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            onAnyTouch(ctx.ReadValue<float>());
+        }
+    }
+}
---
+++ Coffee Stack/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float verticalSpeed = 5f;
+    [SerializeField] float horizontalSpeed = 5f;
+    [SerializeField] private List<GameObject> gameObjects = new List<GameObject>();
+    GameObject mainObject;
+
+    private void OnEnable()
+    {
+        InputManager.onAnyTouch += InputManager_OnAnyTouch;
+    }
+    private void OnDisable()
+    {
+        InputManager.onAnyTouch -= InputManager_OnAnyTouch;
+    }
+    private void Start()
+    {
+        mainObject = transform.GetChild(0).gameObject;
+    }
+    void Update()
+    {
+        transform.position += Vector3.forward * verticalSpeed * Time.deltaTime;
+    }
+    private void InputManager_OnAnyTouch(float input)
+    {
+        rb.AddForce(Vector3.right * input * horizontalSpeed);
+    }
+}
